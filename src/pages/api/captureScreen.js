7c8a05a014da1183ceb6ee1fa71b17812cfc881c
ishakzail <ishak.zail.iz@gmail.com>
2024-05-17 10:26:11 +0100

    feat: back to old version (need to enhance UI)

--- src/pages/api/captureScreen.js
+++ src/pages/api/captureScreen.js
@@ -2,14 +2,17 @@ import puppeteer from 'puppeteer';
 
 export default async function handler(req, res)
 {
-    console.log('dkhel hna');
-    const { url }  = req.body;
-
-    console.log('REQ BODY ==== ', url);
-    // Capture a screenshot of the website using Puppeteer
-    const screenshotPath = await captureScreenshot(url);
-
-    res.status(200).json({ screenshotPath: screenshotPath });
+    try {
+        const { url }  = req.body;
+    
+        // Capture a screenshot of the website using Puppeteer
+        const screenshotPath = await captureScreenshot(url);
+    
+        res.status(200).json({ screenshotPath: screenshotPath });
+        
+    } catch (error) {
+        console.log('error ==== ', error);
+    }
 }
 
 
--- src/pages/api/lighthouse.js
+++ src/pages/api/lighthouse.js
@@ -1,4 +1,6 @@
 import { lighthouseCheck } from "@foo-software/lighthouse-check";
+import https from 'https';
+import http from 'http';
 
 /**
  * Handles the incoming request and performs a series of validations and checks on the URL.
@@ -11,21 +13,21 @@ export default async function handler(req, res)
 {
     const { url, emulatedForm}  = req.body;
     try {
-        // const isValidUrl = validateUrl(url);
-        // if (!isValidUrl || isValidUrl === undefined)
-        //     return res.status(400).json({ error: 'Invalid URL' });
+        const isValidUrl = validateUrl(url);
+        if (!isValidUrl || isValidUrl === undefined)
+            return res.status(400).json({ error: 'Invalid URL' });
 
-        // const urlExists = await checkUrlExists(isValidUrl);
+        const urlExists = await checkUrlExists(isValidUrl);
 
-        // if (!urlExists)
-        //     return res.status(400).json({ error: `Unable to resolve ${isValidUrl}. Verify that the URL is valid.` });
+        if (!urlExists)
+            return res.status(400).json({ error: `Unable to resolve ${isValidUrl}. Verify that the URL is valid.` });
         
         // // Capture a screenshot of the website using Puppeteer
         // const screenshotPath = await captureScreenshot(isValidUrl);
 
         const response = await lighthouseCheck({
             outputDirectory: './public/reports',
-            urls: [url],
+            urls: [isValidUrl],
             emulatedFormFactor: emulatedForm,
         });
 
@@ -36,8 +38,54 @@ export default async function handler(req, res)
         // const reportWebPath = localReportPath.replace('/Users/mac/dev/void-audit-tool-4/public/', '');
         res.status(200).json({ reportPath: reportWebPath })
     } catch (error) {
-        // console.log("error (catch) ----------- ", error);
         res.status(500).json({ error: error })
     }
 }
 
+const validateUrl = (url) => {
+    // Regular expression pattern for a valid URL
+    const urlPattern = /^(http(s)?:\/\/)?(www\.)?([a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)+)(\/.*)?$/;
+
+
+    // Check if the URL matches the pattern
+    const match = url.match(urlPattern);
+    if (match) {
+        // Extract the domain name (including subdomains) from the URL
+        const protocol = match[1];
+        const subdomain = match[3];
+        const domain = match[4];
+        const tld = match[5];
+        const path = match[6];
+        if (domain.startsWith('www.') && url.match(/\./g).length == 1)
+            return null;
+        if (tld !== 'undefined' && subdomain !== 'undefined')
+            if (protocol && path)
+                return `${protocol}${domain}${path}`;
+            else if (protocol === undefined && path)
+                return `https://${domain}${path}`;
+            else
+                return `https://${domain}`;
+    } else {
+        return null;
+    }
+};
+
+
+/**
+ * Checks if a given URL exists by sending a request and checking the HTTP status code.
+ *
+ * @param {string} url - The URL to check.
+ * @return {Promise<boolean>} A promise that resolves to true if the URL exists and has a valid HTTP status code, otherwise false.
+ */
+async function checkUrlExists(url) {
+    const HttpStatusCodes = [200, 301, 302, 307, 308];
+    return new Promise((resolve) => {
+        const protocol = url.startsWith('https') ? https : http;
+        const request = protocol.get(url, (response) => {
+            resolve(HttpStatusCodes.includes(response.statusCode));
+        })
+        request.on('error', () => {
+            resolve(null);
+        });
+    });
+}
